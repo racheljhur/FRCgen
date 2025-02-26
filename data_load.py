bc4ce46e10e3a4ae92e31c4395460ab75808f49c
Rachel Hur <154942153+racheljhur@users.noreply.github.com>
2025-02-26 11:04:09 -0500

    Create data_load.py

---
+++ data_load.py
@@ -0,0 +1,16 @@
+'''pyscript to load in .h5 file containing 900 (751,751) sized SVEs per class'''
+import numpy as np
+import h5py
+
+dat = '900_15_sves.h5'
+
+# class names (access keys)
+pattern_seq = ['F30', 'FA30', 'FAAA30', 'FC30', 'FCCC30',
+                'F40', 'FA40', 'FAAA40', 'FC40', 'FCCC40',
+                'F50', 'FA50', 'FAAA50', 'FC50', 'FCCC50']
+
+# load in .h5 file
+with h5py.File(dat, 'r') as f:
+    for pattern in pattern_seq:
+        data = f[pattern][:]
+        print(f"Loaded {pattern}, data shape: {data.shape}")
