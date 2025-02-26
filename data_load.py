a993e2489343bb7b0a3e291f74572f610a5c742e
Rachel Hur <154942153+racheljhur@users.noreply.github.com>
2025-02-26 11:20:19 -0500

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -3,14 +3,20 @@ import numpy as np
 import h5py
 
 dat = '900_15_sves.h5'
+evs = '900_15_extremevals.h5'
 
 # class names (access keys)
 pattern_seq = ['F30', 'FA30', 'FAAA30', 'FC30', 'FCCC30',
                 'F40', 'FA40', 'FAAA40', 'FC40', 'FCCC40',
                 'F50', 'FA50', 'FAAA50', 'FC50', 'FCCC50']
 
-# load in .h5 file
+# load in .h5 files
 with h5py.File(dat, 'r') as f:
+    for pattern in pattern_seq:
+        data = f[pattern][:]
+        print(f"Loaded {pattern}, data shape: {data.shape}")
+
+with h5py.File(evs, 'r') as f:
     for pattern in pattern_seq:
         data = f[pattern][:]
         print(f"Loaded {pattern}, data shape: {data.shape}")
