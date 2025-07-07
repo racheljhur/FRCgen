35dd3f3830ad6e66832a4bccfe2a786b051896cc
Rachel Hur <154942153+racheljhur@users.noreply.github.com>
2025-07-07 15:20:18 -0400

    Update sve_gen.py

--- sve_gen.py
+++ sve_gen.py
@@ -1,7 +1,7 @@
 """Main FRC SVE Generation Script"""
 
 # author: Daniel Hoover (dhoovr@gmail.com)
-# edited by me (jhur64@gatech.edu) starting from 08_01_24
+# edited by Jihye Hur (jhur64@gatech.edu) starting from 08_01_24
 
 # MIT License
 
@@ -25,53 +25,70 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-
-import os
 import numpy as np
 from pathlib import Path
-from datetime import datetime
-from rich.progress import track
-
-from pathlib import Path
+import os
 from abc import ABC, abstractmethod
-
+import matplotlib.pyplot as plt
+import torch
+from rich.progress import track
 from classes import *
 
-data_dir = Path(os.getcwd()) / "data"
-os.makedirs(data_dir, exist_ok=True)
-
-#---------- generation parameters ----------#
-size = (751, 751)
-radius = 30
-num_structs = 200
-vf = 0.35
-min_dist = 2
-close_dist = min_dist + 2
+#---- generation parameters ----# 
+size = (257, 257)
+radius = 11.29294274
+vf = 0.25
+min_dist = radius + 2
+close_dist = min_dist +2
 percent_far = 0.1
 percent_high_angle = 0.1
 
-#---------- generation pattern ----------#
+circle = make_circle_zero_shift(radius, size)
+
 dist_temp, angle_temp = get_templates(size, radius, min_dist, close_dist)
+pattern = [FarPlacement(min_dist, percent_far)]
+pattern_seq = 'F25'
+num_structs = 650
 
-pattern_string = 'FAAA'
+'''
+FarPlacement(min_dist, percent_far)
+ClosePlacement(min_dist, close_dist),
+AlignedClosePlacement(min_dist, close_dist, percent_high_angle)
+'''
 
-placement_mapping = {
-    'F': lambda: FarPlacement(min_dist, percent_far),
-    'C': lambda: ClosePlacement(min_dist, close_dist),
-    'A': lambda: AlignedClosePlacement(min_dist, close_dist, percent_high_angle),
-}
-
-pattern = [placement_mapping[char]() for char in pattern_string]
-
-#---------- generation loop ----------#
+structs = np.zeros([num_structs, size[0], size[1]])
 centers_list = []
-
 for n in track(range(num_structs)):
-        struct = Structure(size, radius, dist_temp, angle_temp)
-        generator = StructureGenerator(struct, pattern)
-        generator.generate_to_vf(vf)
+    struct_temp = np.zeros(size)
+    struct = Structure(size, radius, dist_temp, angle_temp)
+    StructureGenerator(struct, pattern).generate_to_vf(vf)
+    centers = struct.centers
+    centers_list.append(centers)
+    for center in centers:
+        structs[n, :, :] += np.roll(circle, center, axis=(0, 1))
 
-        centers_list.append(np.hstack([struct.centers, np.ones([len(struct.centers), 1]) * radius]))
+centers_array = np.array(centers_list, dtype=object)
+print('checking centers_array shape:', centers_array.shape)
 
-fiber_centers = np.vstack(centers_list)
-np.save(data_dir / f"{pattern_string}_{num_structs}_centers.npy", fiber_centers)
+#---- if even dimensions are needed... ----#
+
+# new dims
+UNIT_CELL_SIZE = 0.000112099824327958 # d value in um
+GRID_SIZE = 256
+RADIUS = int((4.925824504424278e-06 / UNIT_CELL_SIZE) * GRID_SIZE)  # r (um)/d (um)* d (pixel)
+
+circle = make_circle(RADIUS, GRID_SIZE)
+
+binary_images = []
+for n in range(len(centers_array)):
+    centers = centers_array[n] # get the x,y coords for n^th micro
+
+    binary_image = create_binary_image(centers, circle, GRID_SIZE)
+    binary_images.append(binary_image)
+
+structs_new = np.stack(binary_images)
+
+print("Number of binary images created:", len(binary_images))
+print("Shape of the stacked array:", structs_new.shape)
+
+# np.save(f'micros/{pattern_seq}/{pattern_seq}_{num_structs}_structs.npy', structs_new)
