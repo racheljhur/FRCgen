d7371ef1e9dd43c4ab335d0f1de1dd5f6306ca53
Rachel Hur <154942153+racheljhur@users.noreply.github.com>
2025-11-15 14:18:33 -0500

    Update sve_gen.py

--- sve_gen.py
+++ sve_gen.py
@@ -10,7 +10,6 @@ from classes import *
 
 #---- Global Parameters for Generation ----# 
 '''
-
 The edge length of the domain considered in the study was 112 um.
 The radius was (112 um / 75 pixels) * 11 pixels ≈ 16 um.
 
@@ -22,18 +21,17 @@ THINGS THAT YOU WILL NEED TO CHANGE EXPLICITLY:
 (5) CLOSE CLUSTERING DISTANCES >> minimum_distance + 2
 (6) PERCENTILE OF FAR DISTANCES >> 0.1 (corresponds to the top 99th percentile)
 (7) PERCENTILE OF HORIZONTAL ALIGNMENT >> 0.1 (corresponds to the top 99th percentile)
-
 '''
 
-size = (751, 751) # the dimensions here must be odd
+size = (751, 751) # The dimensions here must be odd
 radius = 30
 vf = 0.30 
 min_dist = radius + 2
 close_dist = min_dist + 2 
 percent_far = 0.1 
 percent_high_angle = 0.1 
-num_structs = 650
-pattern_seq = 'F25' # this is for my filename later
+num_structs = 10 # Number of SVEs to be generated
+pattern_seq = "F30" # This is for my filename later
 
 # Call core generation functions
 '''
@@ -55,7 +53,7 @@ pattern = [FarPlacement(min_dist, percent_far)]
 structs = np.zeros([num_structs, size[0], size[1]])
 # Initialize centers array 
 # (this was mainly for the FE simulations, since the tool we used takes in fiber centers)
-centers_list = []
+# centers_list = []
 
 # Main loop to generate n number of SVEs
 for n in track(range(num_structs)):
@@ -63,19 +61,18 @@ for n in track(range(num_structs)):
     struct = Structure(size, radius, dist_temp, angle_temp)
     StructureGenerator(struct, pattern).generate_to_vf(vf)
     centers = struct.centers
-    centers_list.append(centers)
+    # centers_list.append(centers)
     for center in centers:
         structs[n, :, :] += np.roll(circle, center, axis=(0, 1))
 
-centers_array = np.array(centers_list, dtype=object) # specifying dtype here because python gets annoyed when saving npy arrays of inconsistent number of columns
+# centers_array = np.array(centers_list, dtype=object) # Specifying dtype here because python gets annoyed when saving npy arrays of inconsistent number of columns
 
 # write results
 # np.save(f'micros/{pattern_seq}/{pattern_seq}_{num_structs}_structs.npy', structs)
 # print('checking centers_array shape:', centers_array.shape)
 
-#---- If even dimensions are needed ----#
-
-# Set new dims
+# If even dimensions are needed
+# Setting new dimensions
 UNIT_CELL_SIZE = 0.000112099824327958 # d value in um
 GRID_SIZE = 750
 RADIUS = int((4.925824504424278e-06 / UNIT_CELL_SIZE) * GRID_SIZE)  # r (um)/d (um)* d (pixel)
@@ -84,7 +81,7 @@ circle = make_circle(RADIUS, GRID_SIZE)
 
 binary_images = []
 for n in range(len(centers_array)):
-    centers = centers_array[n] # get the x,y coords for n^th micro
+    centers = centers_array[n] # get the x,y coords for n^th SVE
     binary_image = create_binary_image(centers, circle, GRID_SIZE)
     binary_images.append(binary_image)
 
